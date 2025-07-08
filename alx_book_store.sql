a0f487bf359c708458cfd14469325a14cca168f7
Gacheru <johngacheru533@gmail.com>
2025-07-08 15:56:19 +0300

    other tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,3 +14,34 @@ CREATE TABLE Books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
+CREATE TABLE Customers (
+    customer_id (Primary Key)
+    customer_name VARCHAR(215)
+    email VARCHAR(215)
+    address TEXT    
+)
+-- 1. Create the Customers table
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- 2. Create the Orders table
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- 3. Create the Order_Details table
+CREATE TABLE Order_Details (
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
