902ab7fa1b2ed220e664c7bb0d179de851804463
Gacheru <johngacheru533@gmail.com>
2025-07-09 13:57:01 +0300

    new insert

--- task_5.sql
+++ task_5.sql
@@ -1,7 +1,4 @@
 USE alx_book_store;
 
-sql="INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
-val= ("1","Cole Baidoo","cbaidoo@sandtech.com","123 Happiness Ave.")
-
-mycursor.connect(sql,val)
-mydb.connect(:)
+INSERT INTO Customers(customer_id,customer_name,email,address)
+VALUES (1,'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
